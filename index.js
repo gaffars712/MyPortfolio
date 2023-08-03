0a017bc8d7f9d49eabcd0c1c64a5492696b6f460
gaffars712 <134927774+gaffars712@users.noreply.github.com>
2023-08-03 19:58:59 +0530

    from my laptop
    from my laptop update

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// const time = 1000
+// function updatedata(){
+//     const divwhodelay = document.getElementById("id1st");
+//     divwhodelay.innerHTML = "I'm Gaffar";
+// }
+// setTimeout(updatedata,time);
